074a201623fc2ec893edc55a9e0401af4802e8de
Sarah <sarahyeo2@gmail.com>
2015-03-14 10:12:18 -0700

    Added activities

---
+++ app/src/main/java/com/cloud1001/ecogem/LogActivity.java
@@ -0,0 +1,15 @@
+package com.cloud1001.ecogem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Sarah on 2015-03-14.
+ */
+public class LogActivity extends Activity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/cloud1001/ecogem/VehiclesActivity.java
@@ -0,0 +1,18 @@
+package com.cloud1001.ecogem;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Sarah on 2015-03-14.
+ */
+public class VehiclesActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
