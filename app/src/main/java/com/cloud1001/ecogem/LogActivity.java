fc43d07ac9b4ec6d37c3f4b0c37c36fccf57329d
Jack <jackubc2013@gmail.com>
2015-03-14 17:18:33 -0700

    Minor fixes

--- app/src/main/java/com/cloud1001/ecogem/LogActivity.java
+++ app/src/main/java/com/cloud1001/ecogem/LogActivity.java
@@ -16,7 +16,7 @@ public class LogActivity extends Activity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.log_activity);
+        setContentView(R.layout.activity_log);
         initTextView();
     }
 
